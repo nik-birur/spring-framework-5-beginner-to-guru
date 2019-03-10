f7e1b26ce2e6c08c2eb69cc5d4ae24966536da87
nikhil <nikhil.birur@yahoo.co.in>
2019-03-10 14:57:45 -0500

    Starter project from initializr

---
+++ udemy-spring-5/src/main/java/guru/spring/framework/john/thompson/udemyspring5/UdemySpring5Application.java
@@ -0,0 +1,13 @@
+package guru.spring.framework.john.thompson.udemyspring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemySpring5Application.class, args);
+	}
+
+}
---
+++ udemy-spring-5/src/test/java/guru/spring/framework/john/thompson/udemyspring5/UdemySpring5ApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.spring.framework.john.thompson.udemyspring5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UdemySpring5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
